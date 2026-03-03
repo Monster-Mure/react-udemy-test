f207fa36ae67975a0810b59953e8ba75bfdc50cf
Maheshwar <maheshwar.mure@q2web.com>
2026-03-03 01:35:18 +0100

    add files to git

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Learn with Vite + React</h1>
+     <p>Start editing to see some magic happen!</p>
+    </>
+  
+  )
+}
+
+export default App
