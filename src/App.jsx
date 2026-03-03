5fee2aab8372825e7cd5a3bf4eeb6b540a9ce43f
Maheshwar <maheshwar.mure@q2web.com>
2026-03-03 01:42:30 +0100

    update message in App component for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
      <h1>Learn with Vite + React</h1>
-     <p>Start editing to see some magic happen!</p>
+     <p>Start editing to see some magic happen and change this!</p>
     </>
   
   )
